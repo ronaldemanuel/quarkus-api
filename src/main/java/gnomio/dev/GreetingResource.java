374c4e5b453f392f425772006f069096462e7ff2
ronaldemanuel <emronald15el@gmail.com>
2022-07-14 17:53:18 -0300

    first commit

---
+++ src/main/java/gnomio/dev/GreetingResource.java
@@ -0,0 +1,22 @@
+package gnomio.dev;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class GreetingResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello from RESTEasy Reactive";
+    }
+
+   @POST
+    public Double number() {
+        return 123.0;
+   }
+}
